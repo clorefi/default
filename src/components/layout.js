feec963fbfd0b9333b4e008d92e9c721969fb647
clorefi <89710354+clorefi@users.noreply.github.com>
2021-08-29 23:44:04 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ const Layout = ({ pageTitle, children }) => {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/page-2">Page 2</Link></li>
         </ul>
       </nav>
       <main>
