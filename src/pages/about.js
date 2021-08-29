ff50138c5fed59e5f11c689c94b01c9417398202
clorefi <89710354+clorefi@users.noreply.github.com>
2021-08-29 18:44:37 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
+
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -8,6 +10,7 @@ const AboutPage = () => {
       <title>About Me</title>
       <h1>About Me</h1>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <Link to="/index">homepage</Link>
     </main>
   )
 }
